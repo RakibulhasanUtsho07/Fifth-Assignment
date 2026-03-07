65d12a2feafeac4cbf1ebd8cd67068c2faa255c2
rakibulhasanutsho <162662167+rakibulhasanutsho@users.noreply.github.com>
2026-03-07 12:22:46 +0600

    complete singin part and create hpme page navigation bar and tap container

---
+++ script/index.js
@@ -0,0 +1,18 @@
+console.log("hi ia am js")
+
+const singInBtn = document.getElementById("sing-in-btn");
+const userName = document.getElementById("user-name");
+const userPassword = document.getElementById("user-password");
+
+
+singInBtn.addEventListener("click", function(){
+    if(userName.value === "admin" && userPassword.value === "admin123"){
+        window.location.assign("/home.html");
+        alert("Sing in successfully");
+    }
+    else{
+        alert("User name or User password are not matched")
+    }
+
+
+})
